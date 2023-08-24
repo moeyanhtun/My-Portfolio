7bd7b67b051549f83fa03063e779af3db68a8610
Moe Yan Htun <moeyan227970@gmail.com>
2023-08-24 11:23:40 +0630

    toggle_color

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,11 +8,13 @@ const navMenu = document.getElementById('nav-menu'),
 if (navToggle) {
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
+        navToggle.classList.add('toggle-color')
     })
 }
 if (navClose) {
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
+        navToggle.classList.remove('toggle-color')
     })
 }
 
