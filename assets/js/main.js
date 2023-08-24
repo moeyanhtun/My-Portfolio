4974cd57859e987df8ace0724fb8399ede5bf7ba
Moe Yan Htun <moeyan227970@gmail.com>
2023-08-25 02:48:30 +0630

    scrollUp

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,34 @@
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
     navToggle = document.getElementById('nav-toggle'),
-    navClose = document.getElementById('nav-close')
+    navClose = document.getElementById('nav-close'),
+    hoverRemove = document.getElementById('toggle');
 
     // menu show
 
 if (navToggle) {
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
-        navToggle.classList.add('toggle-color')
+  
+        hoverRemove.classList.add('remoevI')
     })
 }
 if (navClose) {
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
-        navToggle.classList.remove('toggle-color')
+
+        hoverRemove.classList.remove('remoevI')
     })
 }
 
 
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link')
+
 const linkAction = () => {
     const navMenu = document.getElementById('nav-menu')
-        navMenu.classList.remove('show-menu')
+    navMenu.classList.remove('show-menu')
+     navToggle.classList.remove('toggle-color')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 /*=============== SHADOW HEADER ===============*/
@@ -55,12 +60,45 @@ const sendEmail = (e) => {
     })
 }
 contactForm.addEventListener('submit', sendEmail)
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
 
 
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollUp = document.getElementById("scroll-up");
+  
+    // Show the button when user scrolls down
+    window.onscroll = function () {
+        if (document.body.scrollTop > 380 || document.documentElement.scrollTop > 380) {
+            scrollUp.style.display = "block";
+        } else {
+            scrollUp.style.display = "none";
+        }
+    };
+  
+    // Scroll to the top when the button is clicked
+    scrollUp.addEventListener("click", function () {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+    });
+});
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+    sections.forEach(current => {
+        const sectionHegiht = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHegiht) {
+            sectionClass.classList.add('active-link')
+        } else {
+            sectionClass.classList.remove('active-link')
+        }
 
-
+    })
+}
+window.addEventListener('scroll', scrollActive)
 /*=============== DARK LIGHT THEME ===============*/ 
 
 
